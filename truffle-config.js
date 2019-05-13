92aa015e68dd0c10de258f6ca7bae61b9744bee8
hideno <hidenoryo@lifull.com>
2019-05-13 17:23:27 +0900

    modify truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "172.17.0.2",
+      port: 8545,
       network_id: "*",
+      gas: 4600000,
     },
   },
   mocha: {
